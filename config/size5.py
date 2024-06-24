feb6ab2e3ef617a38f7819f4b99acd828214b7cc
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-06-24 14:27:50 -0500

    Update size5.py

--- config/size5.py
+++ config/size5.py
@@ -19,9 +19,9 @@ batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
+n_layer = 1
+n_head = 2
+n_embd = 2048
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
